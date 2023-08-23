f2341c696a63e15cc2b8371239a4c74794ef5cce
Khang Pham <khang.p@sharering.network>
2023-08-23 17:27:36 +0700

    init

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
-fn main() {
-    let y = {
-        let x: i32 = 3;
-        x + 1
-    };
-    print_labeled_measurement(y, 'h');
-    print_labeled_measurement(five(), 'h')
-}
-fn five() -> i32 {
-    5
-}
-fn print_labeled_measurement(value: i32, unit_label: char) {
-    println!("The measurement is: {value}{unit_label}");
+#[macro_use]
+extern crate actix_web;
+
+use std::{io,env};
+use actix_web::{HttpServer, App,middleware};
+
+mod tweet;
+mod like;
+mod response;
+mod constants;
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+  
+  env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+  env_logger::init();
+  HttpServer::new(|| {
+      App::new()
+          // enable logger - always register actix-web Logger middleware last
+          .wrap(middleware::Logger::default())
+          // register HTTP requests handlers
+          .service(tweet::list)
+          .service(tweet::get)
+          .service(tweet::create)
+          .service(tweet::delete)
+          // .service(like::list)
+          // .service(like::plus_one)
+          // .service(like::minus_one)
+  })
+  .bind("0.0.0.0:9090")?
+  .run()
+  .await
 }
